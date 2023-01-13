cd77ca2d41668a9eeb8acc2825aa0f242e9c1fd0
OctoberDavid <2487602324@qq.com>
2023-01-13 14:56:08 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/david/git/HelloGit.java
@@ -0,0 +1,15 @@
+package com.david.git;
+
+/**
+ * ClassName: HelloGit
+ * Package: com.david.git
+ * Description:
+ *
+ * @Author: Aftern0on
+ * @Create: 2023/1/13 - 14:17
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
